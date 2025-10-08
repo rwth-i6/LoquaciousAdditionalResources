1f5f72a9ab4af0bdf411cb5b6d3877beae91061b
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2025-10-08 14:28:48 +0200

    rename variables

--- 01_prepare_cmudict.sh
+++ 01_prepare_cmudict.sh
@@ -18,4 +18,4 @@ echo "$SHA256 cmudict-0.7b"  | sha256sum  -c
 cat cmudict-0.7b | grep -v "^;;;" | grep "^[a-zA-Z'][a-zA-Z0-9' ()]*$" | sed 's/([0-9])//g' | grep -v "^[A-Z0-9']*[0-9][A-Z0-9']* " | sed 's/[0-9]//g' > cmudict.g2p-train.txt
 
 # Extract only the words for later usage
-cat cmudict.g2p-train.txt | cut -d' ' -f1 | sort | uniq | sort > cmudict-words.txt
+cat cmudict.g2p-train.txt | cut -d' ' -f1 | sort | uniq | sort > cmudict.words.txt
--- 04_create_vocab.sh
+++ 04_create_vocab.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -vex
 
-#cat loquacious-large.txt | tr ' ' '\n' | sort | uniq -c > train-counts.txt
-cat train-counts.txt | awk '{print $2}' | sort > train-words.txt
+cat loquacious.txt | tr ' ' '\n' | sort | uniq -c > loquacious.counts.txt
+cat loquacious.counts.txt | awk '{print $2}' | sort > loquacious.words.txt
 
 # extract words that appear both in the training data and in cmudict
-comm -12 train-words.txt cmudict-words.txt > cmudict-train-intersection-words.txt
-cat train-counts.txt | awk '{if($1>=4){print $2}}' > train-min-4-occurences-words.txt
-cat cmudict-train-intersection-words.txt train-min-4-occurences-words.txt | sort | uniq > loquacious-vocab.txt
-comm -23 loquacious-vocab.txt cmudict-words.txt > nondict-words.txt
+comm -12 loquacious.words.txt cmudict.words.txt > cmudict.loquacious-intersection-words.txt
+cat loquacious.counts.txt | awk '{if($1>=4){print $2}}' > loquacious.min-4-occurences-words.txt
+cat cmudict.loquacious-intersection-words.txt loquacious.min-4-occurences-words.txt | sort | uniq > loquacious.vocab.txt
+comm -23 loquacious.vocab.txt cmudict.words.txt > nondict.words.txt
 
-g2p.py -e utf-8 -V 1.0 --variants-number 2 -m g2p_model_5 -a nondict-words.txt > nondict-words.g2p.txt
+g2p.py -e utf-8 -V 1.0 --variants-number 2 -m g2p_model_5 -a nondict.words.txt > nondict.g2p.txt
