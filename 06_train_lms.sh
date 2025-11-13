8f7f1ba8bb47b86eb44c944662f75d879a64ef85
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2025-11-13 09:36:44 +0100

    remove explicit apptainer calls from 06_train_lms.sh

--- 06_train_lms.sh
+++ 06_train_lms.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-PWD=$(pwd)
-BIND=$(realpath $PWD)
 if [ ! -f 3gram-pruned.arpa.gz ]; then
-    apptainer run -B $BIND apptainer.sif kenlm/build/bin/lmplz -o 3 -S 25% -T /var/tmp --prune "001" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 3gram-pruned.arpa.gz
+    kenlm/build/bin/lmplz -o 3 -S 25% -T /var/tmp --prune "001" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 3gram-pruned.arpa.gz
 fi
 if [ ! -f 3gram-unpruned.arpa.gz ]; then
-    apptainer run -B $BIND apptainer.sif kenlm/build/bin/lmplz -o 3 -S 25% -T /var/tmp --prune "000" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 3gram-unpruned.arpa.gz
+    kenlm/build/bin/lmplz -o 3 -S 25% -T /var/tmp --prune "000" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 3gram-unpruned.arpa.gz
 fi
 if [ ! -f 4gram-pruned.arpa.gz ]; then
-    apptainer run -B $BIND apptainer.sif kenlm/build/bin/lmplz -o 4 -S 25% -T /var/tmp --prune "0011" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 4gram-pruned.arpa.gz
+    kenlm/build/bin/lmplz -o 4 -S 25% -T /var/tmp --prune "0011" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 4gram-pruned.arpa.gz
 fi
 if [ ! -f 4gram-unpruned.arpa.gz ]; then
-    apptainer run -B $BIND apptainer.sif kenlm/build/bin/lmplz -o 4 -S 25% -T /var/tmp --prune "0000" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 4gram-unpruned.arpa.gz
+    kenlm/build/bin/lmplz -o 4 -S 25% -T /var/tmp --prune "0000" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 4gram-unpruned.arpa.gz
 fi
