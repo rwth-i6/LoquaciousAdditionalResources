9e70c121251d485e05e002f9bfb7ffa345ad0713
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2025-11-21 16:05:05 +0100

    correct leftover incorrect pruning format

--- 06_train_lms.sh
+++ 06_train_lms.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ ! -f 3gram-pruned.arpa.gz ]; then
-    kenlm/build/bin/lmplz -o 3 -S 25% -T /var/tmp --prune "001" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 3gram-pruned.arpa.gz
+    kenlm/build/bin/lmplz -o 3 -S 25% -T /var/tmp --prune 0 0 1 --limit_vocab_file loquacious.vocab.txt --text loquacious-large.txt | gzip > 3gram-pruned.arpa.gz
 fi
 if [ ! -f 3gram-unpruned.arpa.gz ]; then
-    kenlm/build/bin/lmplz -o 3 -S 25% -T /var/tmp --prune "000" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 3gram-unpruned.arpa.gz
+    kenlm/build/bin/lmplz -o 3 -S 25% -T /var/tmp --prune 0 0 0 --limit_vocab_file loquacious.vocab.txt --text loquacious-large.txt | gzip > 3gram-unpruned.arpa.gz
 fi
 if [ ! -f 4gram-pruned.arpa.gz ]; then
-    kenlm/build/bin/lmplz -o 4 -S 25% -T /var/tmp --prune "0011" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 4gram-pruned.arpa.gz
+    kenlm/build/bin/lmplz -o 4 -S 25% -T /var/tmp --prune 0 0 1 1 --limit_vocab_file loquacious.vocab.txt --text loquacious-large.txt | gzip > 4gram-pruned.arpa.gz
 fi
 if [ ! -f 4gram-unpruned.arpa.gz ]; then
-    kenlm/build/bin/lmplz -o 4 -S 25% -T /var/tmp --prune "0000" --limit_vocab_file loquacious-vocab.txt --text loquacious-large.txt | gzip > 4gram-unpruned.arpa.gz
+    kenlm/build/bin/lmplz -o 4 -S 25% -T /var/tmp --prune 0 0 0 0 --limit_vocab_file loquacious.vocab.txt --text loquacious-large.txt | gzip > 4gram-unpruned.arpa.gz
 fi
